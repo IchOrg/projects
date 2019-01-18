ad1a86ea856198db0ca7bfc226ecf910e38bb601
Oshurkevich Roman <oshurkevichroman@yandex.ru>
2019-01-18 22:31:26 +0400

    added simple todo list

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
 window.onload = function(){
     console.log('simple text');
 }
+
+let form = document.getElementsByTagName('form')[0];
+form.addEventListener('submit',addTask);
+
+function addTask(e){
+    e.preventDefault();
+    let textInput = document.getElementById('sendtask');
+    let ul = document.getElementsByClassName('list-task')[0];
+    let value = textInput.value;
+    let newTask = document.createElement('li');
+    newTask.innerHTML = value;
+    ul.appendChild(newTask);
+    textInput.value = '';
+}
